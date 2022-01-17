020ec5a81c935e7618efffaacf03b37d2ecdc8f1
alamodi <abdullah.alamodi@bootfi.com>
2022-01-17 18:37:58 +0300

    add search feature

--- lib/src/constants/app_colors.dart
+++ lib/src/constants/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const rainBlueLight = Color(0xFF4480C6);
   static const rainBlueDark = Color(0xFF364699);
   static const rainGradient = [rainBlueLight, rainBlueDark];
+  static const accentColor = Color(0xFFe96e50);
 }
