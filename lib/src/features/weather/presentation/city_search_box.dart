0815103d2377b28db21d20826e54d1cd75f9434d
Andrea Bizzotto <codewithandrea@gmail.com>
2023-11-23 14:12:53 +0000

    Tweak CitySearchBox

--- lib/src/features/weather/presentation/city_search_box.dart
+++ lib/src/features/weather/presentation/city_search_box.dart
@@ -31,12 +31,12 @@ class _CitySearchRowState extends ConsumerState<CitySearchBox> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: SizedBox(
-              height: 50,
+      child: SizedBox(
+        height: _radius * 2,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
               child: TextField(
                 controller: _searchController,
                 textAlign: TextAlign.center,
@@ -46,38 +46,38 @@ class _CitySearchRowState extends ConsumerState<CitySearchBox> {
                   filled: true,
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(_radius),
-                        bottomLeft: Radius.circular(_radius)),
+                      topLeft: Radius.circular(_radius),
+                      bottomLeft: Radius.circular(_radius),
+                    ),
                   ),
                 ),
                 onSubmitted: (value) =>
                     ref.read(cityProvider.notifier).state = value,
               ),
             ),
-          ),
-          InkWell(
-            child: Container(
-              height: 50,
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                color: AppColors.accentColor,
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(_radius),
-                  bottomRight: Radius.circular(_radius),
+            InkWell(
+              child: Container(
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  color: AppColors.accentColor,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(_radius),
+                    bottomRight: Radius.circular(_radius),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Text('search',
+                      style: Theme.of(context).textTheme.bodyLarge),
                 ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Text('search',
-                    style: Theme.of(context).textTheme.bodyLarge),
-              ),
-            ),
-            onTap: () {
-              FocusScope.of(context).unfocus();
-              ref.read(cityProvider.notifier).state = _searchController.text;
-            },
-          )
-        ],
+              onTap: () {
+                FocusScope.of(context).unfocus();
+                ref.read(cityProvider.notifier).state = _searchController.text;
+              },
+            )
+          ],
+        ),
       ),
     );
   }
